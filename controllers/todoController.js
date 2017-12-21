36705feb6c4b1fcc12aea677be0496ad4f351e57
Longears00 <Melani_y@126.com>
2017-12-21 17:35:00 +0800

    todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,6 +2,10 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var todoSchema = new mongoose.Schema({
+    item: String,
+  });
+var Todo = mongoose.model('Todo', todoSchema);
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var data = [{
     item: 'get milk',
@@ -15,15 +19,21 @@ var data = [{
 
 module.exports = function (app) {
   app.get('/todo', (req, res)=> {
-    res.render('todo', { todo: data });
-  });
+      Todo.find({}, (err, data)=> {
+        if (err)throw err;
+        res.render('todo', { todo: data });
+      });
+    });
   app.post('/todo', urlencodedParser, (req, res)=> {
-    data.push(req.body);
-    res.json(data);
+    var newTodo = Todo(req.body).save((err, data)=> {
+        if (err)throw err;
+        res.json(data);
+      });
   });
   app.delete('/todo/:item', (req, res)=> {
-    data = data.filter(function (todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+      Todo.find({ item: req.params.item.replace(/-/g, ' ') }).remove((err, data)=> {
+          if (err)throw err;
+          res.json(data);
+        });
     });
-  });
 };
